c443139ae9293c2f478b03798d59b4bde36a5002
Anmol <anmolseth00@gmail.com>
2023-12-05 00:15:08 -0500

    Updating test cases

--- tests/testthat/test-plot_countries_histogram.R
+++
@@ -1,8 +0,0 @@
-test_that("plot_countries_frequency() generates a histogram", {
-  library(ggplot2)
-  data("countries")
-  countries <- countries |> rename(country_name = name)
-  vdiffr::expect_doppelganger(
-    "countries_histogram", plot_countries_frequency(countries, 5)
-  )
-})
