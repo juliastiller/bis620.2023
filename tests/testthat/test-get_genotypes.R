64aaccce8e1a51717891543c4ad466c86cf9d0ae
juliastiller <148993755+juliastiller@users.noreply.github.com>
2023-12-20 18:26:01 -0500

    Add files via upload

--- tests/testthat/test-get_genotypes.R
+++ tests/testthat/test-get_genotypes.R
@@ -1,5 +1,5 @@
 test_that("get_genotypes() generates a string", {
   expect_equal(get_genotype("000001"), "Unknown")
   expect_equal(get_genotype("000003"), "Mutant")
-  expect_equal(get_genotype("000018"), "Mutant")
+  expect_equal(get_genotype("000018"), "Wild-type")
 })
