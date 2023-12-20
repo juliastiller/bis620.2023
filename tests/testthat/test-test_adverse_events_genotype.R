8589eec27343ac489813b5b1192cdc038f294e9a
juliastiller <148993755+juliastiller@users.noreply.github.com>
2023-12-20 17:51:07 -0500

    Add files via upload

--- tests/testthat/test-test_adverse_events_genotype.R
+++ tests/testthat/test-test_adverse_events_genotype.R
@@ -1,4 +1,4 @@
 test_that("test_adverse_events_genotype() generates a t-test", {
   data("d")
-  expect_equal(type(test_adverse_events_genotype(d)), "htest")
+  expect_equal(class(test_adverse_events_genotype(d)), "htest")
 })
